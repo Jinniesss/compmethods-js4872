23c8f71778da684f3c53d1062a8bb494f40fa7f2
Jinnie <ssgml@foxmail.com>
2025-12-05 15:59:37 -0500

    Start new project.

--- project/first_glance.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import tqdm
-
-file_name = 'project/data/NSDUH_2023_Tab.txt'
-df = pd.read_csv(file_name, sep="\t", dtype=str)
-# print(df.head())
-print(df['AGE3'].value_counts())
